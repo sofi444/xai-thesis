dd3db786b27d54524415b18c280e3130a3434813
Sofia Casadei (FG-221) <Sofia.Casadei@bmw.de>
2023-08-16 01:34:30 +0200

    improved output parsing

--- json_parser.py
+++ json_parser.py
@@ -21,14 +21,17 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 
-def load_responses(filename:str="test_json.json", full_run:bool=False):
+def load_freetext_responses(filename:str="test_json.json", full_run:bool=False):
     filepath = os.path.join(PROJECT_DIR, "responses", filename)
     
     with open(filepath, "r") as f:
         responses = json.load(f)
+        
         if not full_run:
-            N = 200 # tmp
+            N = 20 # tmp
             responses = {idx:res for idx, res in responses.items() if int(idx) < N}
+            #interesting_cases = [14,17,53]
+            #responses = {idx:res for idx,res in responses.items() if int(idx) in interesting_cases}
     
     return responses
 
@@ -49,8 +52,32 @@ def write_formatted_responses(formatted_responses:dict, out_filename:str):
 
 
 
+def load_template(template_version:str):
+    template_filepath = os.path.join(PROJECT_DIR, 
+                                     "prompt_templates/output-parsing_templates.json")
+    with open(template_filepath, "r") as f:
+        templates = json.load(f)
+        template = templates[template_version]
+    
+    return template
+
+
+
+def catch_errors(formatted_response:dict):
+    ''' Add ERROR key to formatted_response if errors are found '''
+
+    # multiple answers
+    if len(formatted_response["answer_letter"]) > 1:
+        formatted_response["ERROR"] = "multiple answers"
+    if formatted_response["answer_letter"] == "":
+        formatted_response["ERROR"] = "no answer"
+    
+    return formatted_response
+
+
+
 def main(args):
-    responses = load_responses(args.in_filename, args.full_run)
+    responses = load_freetext_responses(args.in_filename, args.full_run)
 
     env_dict = config_env()
     
@@ -65,22 +92,31 @@ def main(args):
                                     use_azure=args.use_azure,
                                     model=args.model)
 
-    template = """
-    Convert the text into json. \n\nText: {text} \n\n {format_instructions}
-    """
-    template = PromptTemplate.from_template(template)
-    output_parser, format_instructions = utils.output.get_format_instructions()
+    template = PromptTemplate.from_template(load_template(template_version="v3"))
 
+    output_parser, format_instructions = utils.output.build_parser(schemas_version="v3",
+                                                                   parser_type="structured",
+                                                                   only_json=False)
+    
     formatted_responses = {}
     for idx, response in tqdm.tqdm((responses.items())):
-        prompt = template.format(text=response, 
+        prompt = template.format(freetext_response=response, 
                                  format_instructions=format_instructions)
-        formatted_response = model.predict(prompt)
+        formatted_response = model.predict(prompt) # str
+
         try:
-            formatted_response = output_parser.parse(formatted_response)
+            formatted_response = output_parser.parse(formatted_response) # dict
+            formatted_response = catch_errors(formatted_response)
         except:
-            print(formatted_response, type(formatted_response))
+            formatted_responses[idx] = {"ERROR": "unable to parse",
+                                        "output_string": formatted_response,
+                                        "full_text": response}
             continue
+
+        # add full_text field to with original free text response
+        if "full_text" not in formatted_response.keys():
+            formatted_response["full_text"] = response
+
         formatted_responses[idx] = formatted_response
 
     write_formatted_responses(formatted_responses=formatted_responses,
@@ -88,41 +124,29 @@ def main(args):
 
 
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
-    parser.add_argument(
-        "--model",
-        type=str,
-        default="OpenAI-chat-default",
-        help="model to use",
-    )
-
-    parser.add_argument(
-        "--in_filename",
-        type=str,
-        default="test_json.json",
-        help="file with the free-text responses",
-    )
-
-    parser.add_argument(
-        "--out_filename",
-        type=str,
-        default="formatted_responses.json",
-        help="file where to save the formatted responses",
-    )
-
-    parser.add_argument(
-        "--use_azure",
-        action='store_true',
-        help="include --use_azure to use OpenAI models from Azure, omit otherwise",
-    )
-
-    parser.add_argument(
-        "--full_run",
-        action='store_true',
-        help="include --use_azure to run on all instances from the datafile, omit to run on N instance (set N)",
-    )
+    parser.add_argument("--model",
+                        type=str,
+                        default="OpenAI-chat-default",
+                        help="model to use")
+    parser.add_argument("--in_filename",
+                        type=str,
+                        default="test_json.json",
+                        help="file with the free-text responses")
+    parser.add_argument("--out_filename",
+                        type=str,
+                        default="formatted_responses.json",
+                        help="file where to save the formatted responses")
+    parser.add_argument("--use_azure",
+                        action='store_true',
+                        help="include to use OpenAI models from Azure, omit otherwise")
+    parser.add_argument("--full_run",
+                        action='store_true',
+                        help="include to run on all instances from the datafile, omit to run on N instance (set N)")
 
     args = parser.parse_args()
     main(args)
--- utils/output.py
+++ utils/output.py
@@ -1,117 +1,95 @@
 
 import json
-import datetime
+import datetime as dt
 import os
 
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser, PydanticOutputParser
+
+from pydantic import BaseModel, Field, validator
 
 
 
-def _get_schemas(schemas_src:str="default"):
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    if schemas_src == "default":
 
-        prediction_letter_schema = ResponseSchema(
-            name="prediction_letter", 
-            description="This is the answer to the question. It is one letter (either A, B, C, D, or E), which corresponds to the answer chosen by the model."
-        )
 
-        prediction_text_schema = ResponseSchema(
-            name="prediction_text", 
-            description="This is the answer to the question. It is the text of the answer chosen by the model."
-        )
-
-        explanation_schema = ResponseSchema(
-            name="explanation", 
-            description="This is the explanation for the prediction. It is a piece of text explains why the model chose the answer it did."
-        )
+def load_schemas(schemas_version:str):
+    schemas_filepath = os.path.join(PROJECT_DIR,
+                                    "schemas/output-parsing_schemas.json")
+    with open(schemas_filepath, "r") as f:
+        schemas = json.load(f)
+        schemas = schemas[schemas_version]
     
-        schemas = [prediction_letter_schema, prediction_text_schema, explanation_schema]
-        return schemas
+    return schemas
+
+
+
+def get_langchain_schemas(schemas):
+    return [ResponseSchema(name=k, description=v) for k,v in schemas.items()]
+
+
+
+class PydanticResponse(BaseModel): # not in use
+
+    answer_letter: str = Field(description="the letter corresponding to the final answer.")
+    answer_text: str = Field(description="the text corresponding to the final answer.")
+
+    # You can add custom validation logic easily with Pydantic.
+    @validator("answer_letter")
+    def is_valid_choice(cls, field):
+        if field not in ["A", "B", "C", "D", "E"]:
+            raise ValueError("Not a valid choice")
+        return field
     
-    else:
-        raise NotImplementedError
 
 
-
-def _create_output_parser(schemas, type:str="structured"):
-
-    if type == "structured":
-        return StructuredOutputParser.from_response_schemas(schemas)
-    else:
-        raise NotImplementedError
-
-
-
-def get_format_instructions(schemas_src:str="default", parser_type:str="structured", only_json:bool=True):
+def build_parser(schemas_version:str="v2", parser_type:str="structured", only_json:bool=False):
     """
     Retrieves schemas, initializes output parser, and returns the parse and format instructions.
 
     only_json (bool): If True, only the json in the Markdown code snippet will be returned, 
     without the introducing text (the output should be a Markdown code ...). Defaults to False.
     """
-    schemas = _get_schemas(schemas_src=schemas_src)
-    output_parser = _create_output_parser(schemas=schemas,
-                                         type=parser_type)
-    format_instructions = output_parser.get_format_instructions(only_json=only_json)
+
+    schemas = get_langchain_schemas(load_schemas(schemas_version))
+
+    if parser_type == "structured":
+        output_parser = StructuredOutputParser.from_response_schemas(schemas)
+        format_instructions = output_parser.get_format_instructions(only_json=only_json)
+
+    elif parser_type == "pydantic": # not in use
+        raise NotImplementedError("Use structured parser")
+        output_parser = PydanticOutputParser(pydantic_object=PydanticResponse)
+        format_instructions = output_parser.get_format_instructions()
 
     return output_parser, format_instructions
 
 
 
-def write_responses(responses):
+def write_responses(responses, idx_uuid_map, save_uuids:bool=False):
     """
     Write responses (dict) to a JSON file.
     Suitable for small experimental runs.
     """
-
-    time_now = datetime.datetime.now()
-    time_now_str = time_now.strftime("%d%m_%H%M")
-    filename = f"responses_{time_now_str}.json"
-
-    PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    filepath = os.path.join(PROJECT_DIR, "outputs", filename)
+    time_now = dt.datetime.now().strftime("%d%m%H%M")
+    filename = f"responses_{time_now}.json"
+    filepath = os.path.join(PROJECT_DIR, "responses", filename)
 
     with open(filepath, "w") as f:
-        json.dump(responses, f, indent=4)
+       json.dump(responses, f, indent=4)
 
-
+    if save_uuids:
+        filepath_uuids = os.path.join(PROJECT_DIR, "processed_uuids", f"uuids_{time_now}.json")
+        with open(filepath_uuids, "w+") as f:
+            json.dump(idx_uuid_map, f, indent=4)
 
 
 
 
 if __name__ == "__main__":
 
-    responses = {0: {'explanation': 'A revolving door is convenient for two direction travel, '
-                    'but it also serves as a security measure at a bank. Banks '
-                    'often use revolving doors to control the flow of people '
-                    'entering and exiting the building, as well as to provide '
-                    'an added layer of security by limiting access to the '
-                    'interior of the bank.',
-                    'prediction_letter': 'A',
-                    'prediction_text': 'bank'},
-                1: {'explanation': 'People aim to complete their job at work. This is the '
-                                    'primary goal of most individuals in a professional '
-                                    'setting. While learning from each other and talking to '
-                                    'each other may also occur at work, the main objective is '
-                                    'to complete the tasks and responsibilities assigned to '
-                                    'them.',
-                    'prediction_letter': 'A',
-                    'prediction_text': 'complete job'},
-                2: {'explanation': 'The correct answer is B: bookstore. Bookstores typically '
-                                    'have a wide range of printed works, including magazines, '
-                                    'available for purchase.',
-                    'prediction_letter': 'B',
-                    'prediction_text': 'bookstore'}}
-    
-    write_responses(responses)
-    exit()
-
-    format_instructions = get_format_instructions(
-        schemas_src="default",
-        parser_type="structured",
-        only_json=True
-    )
+    output_parser, format_instructions = build_parser(parser_type="structured")
 
     print(format_instructions)
 
