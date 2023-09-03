a58e014f9f5b7a7e47e01468e15e45566314db09
sofi444 <sofyasr22@icloud.com>
2023-09-03 16:43:49 +0200

    codellama batch call - not working

--- freetext_to_format.py
+++ freetext_to_format.py
@@ -7,6 +7,7 @@ import tqdm
 
 import utils.output
 import utils.models
+import utils.codellama
 
 from langchain import PromptTemplate
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
@@ -18,20 +19,27 @@ from dotenv import load_dotenv, find_dotenv, dotenv_values
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+RESPONSES_DIR = os.path.join(PROJECT_DIR, 'responses')
 
 
 
-def load_freetext_responses(filename:str="test_json.json", full_run:bool=False):
-    filepath = os.path.join(PROJECT_DIR, "responses", filename)
+def load_freetext_responses(filename:str, full_run:bool=False):
+    filepath = os.path.join(RESPONSES_DIR, filename)
+    N = 7
+
+    if filename.endswith(".json"):
+        # Format: {idx: freetext response, ...}
+        with open(filepath, "r") as f:
+            responses = json.load(f)
+            if not full_run:
+                responses = {idx:res for idx, res in responses.items() if int(idx) < N}
     
-    with open(filepath, "r") as f:
-        responses = json.load(f)
-        
-        if not full_run:
-            N = 20 # tmp
-            responses = {idx:res for idx, res in responses.items() if int(idx) < N}
-            #interesting_cases = [14,17,53]
-            #responses = {idx:res for idx,res in responses.items() if int(idx) in interesting_cases}
+    elif filename.endswith(".jsonl"):
+        # Format: [{uuid: uuid, id: idx, response: freetext response}, ...]
+        with open(filepath, "r") as f:
+            responses = [json.loads(line) for line in f.readlines()]
+            if not full_run:
+                responses = responses[:N]
     
     return responses
 
@@ -77,10 +85,10 @@ def catch_errors(formatted_response:dict):
 
 
 def main(args):
-    env_dict = config_env()
     responses = load_freetext_responses(args.in_filename, args.full_run)
 
-    if model == "openai_chat":
+    if args.model == "openai_chat":
+        env_dict = config_env()
         if args.use_azure:
             openai.api_type = "azure"
             openai.api_base = env_dict["AZURE_OPENAI_ENDPOINT"]
@@ -92,39 +100,112 @@ def main(args):
                                         use_azure=args.use_azure,
                                         model=args.model)
     
-    elif model == "codellama":
-        model, tokenizer =
-    
+    elif args.model == "codellama":
+        #model, tokenizer = utils.models.load_model(model=args.model)
+        model, tokenizer = utils.codellama.load_codellama()
 
     template = PromptTemplate.from_template(load_template(template_version="v3"))
 
-    output_parser, format_instructions = utils.output.build_parser(schemas_version="v3",
-                                                                   parser_type="structured",
-                                                                   only_json=False)
+    output_parser, format_instructions = utils.output.build_parser(
+        schemas_version="v3",
+        parser_type="structured",
+        only_json=False
+    )
     
-    formatted_responses = {}
-    for idx, response in tqdm.tqdm((responses.items())):
-        prompt = template.format(freetext_response=response, 
-                                 format_instructions=format_instructions)
-        formatted_response = model.predict(prompt) # str
+    if args.model == "codellama":
+        inputs = []
+        batch_size = 3
+        formatted_responses = {}
+        main_idx = 0
 
-        try:
-            formatted_response = output_parser.parse(formatted_response) # dict
-            formatted_response = catch_errors(formatted_response)
-        except:
-            formatted_responses[idx] = {"ERROR": "unable to parse",
-                                        "output_string": formatted_response,
-                                        "full_text": response}
-            continue
+        instructions, examples = utils.codellama.tokenize_instructions_and_examples(
+            few_shot=False,
+            tokenizer=tokenizer,
+            instructions=format_instructions
+        )
 
-        # add full_text field to with original free text response
-        if "full_text" not in formatted_response.keys():
-            formatted_response["full_text"] = response
+        for response in responses:
+            if isinstance(response, dict): # batch responses jsonl
+                inputs.append(response["response"])
+            elif isinstance(response, str): # single response json
+                inputs.append(response)
+            
+            if len(inputs) == batch_size or len(responses)-main_idx < batch_size:
+                inputs = utils.codellama.tokenize_inputs(
+                    inputs=inputs,
+                    tokenizer=tokenizer
+                )
+                batch_inputs = utils.codellama.create_batch_inputs(
+                    instructions_tok=instructions,
+                    examples_tok=examples,
+                    inputs_tok=inputs
+                )
+                batch_outputs = utils.codellama.run_batch(
+                    model=model,
+                    batch_encoded=batch_inputs
+                )
+                batch_outputs_decoded = utils.codellama.decode_batch(
+                    batch_outputs=batch_outputs,
+                    batch_inputs=batch_inputs,
+                    tokenizer=tokenizer
+                )
+                
+                batch_formatted = []
+                for idx_batch, output in enumerate(batch_outputs_decoded):
+                    try:
+                        formatted = output_parser.parse(output) # dict
+                        formatted = catch_errors(output) # dict
+                    except:
+                        formatted = {"ERROR": "unable to parse",
+                                     "parser_output": output}
+                    
+                    if isinstance(response, dict): # batch responses jsonl
+                        # add keys from original response
+                        formatted['idx'] = response["idx"]
+                        formatted['uuid'] = response["uuid"]
+                        # add full text
+                        formatted["full_text"] = inputs[idx_batch]
+                    
+                    elif isinstance(response, str): # single response json
+                        formatted["full_text"] = inputs[idx_batch]
+                        formatted["idx"] = main_idx
 
-        formatted_responses[idx] = formatted_response
+                    batch_formatted.append(formatted)
+                    main_idx += 1
 
-    write_formatted_responses(formatted_responses=formatted_responses,
-                              out_filename=args.out_filename)
+                inputs = [] # reset
+
+                print(batch_formatted)
+                print(main_idx)
+
+
+
+    elif args.model == "openai":
+
+        # structure of free text responses
+        formatted_responses = {}
+        for idx, response in tqdm.tqdm((responses.items())):
+            prompt = template.format(freetext_response=response, 
+                                    format_instructions=format_instructions)
+            formatted_response = model.predict(prompt) # str
+
+            try:
+                formatted_response = output_parser.parse(formatted_response) # dict
+                formatted_response = catch_errors(formatted_response)
+            except:
+                formatted_responses[idx] = {"ERROR": "unable to parse",
+                                            "output_string": formatted_response,
+                                            "full_text": response}
+                continue
+
+            # add full_text field to with original free text response
+            if "full_text" not in formatted_response.keys():
+                formatted_response["full_text"] = response
+
+            formatted_responses[idx] = formatted_response
+
+        write_formatted_responses(formatted_responses=formatted_responses,
+                                out_filename=args.out_filename)
 
 
 
@@ -139,6 +220,7 @@ if __name__ == "__main__":
                         help="model to use for parsing: openai_chat | codellama")
     parser.add_argument("--in_filename",
                         type=str,
+                        default="freetext_turbo0301_700dev_14081857.json",
                         help="file with the free-text responses")
     parser.add_argument("--out_filename",
                         type=str,
--- utils/codellama.py
+++ utils/codellama.py
@@ -4,16 +4,16 @@ import json
 import torch
 
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers.tokenization_utils_base import BatchEncoding
+
 from langchain import PromptTemplate
 
-import prompting
-import output
 
 
-
-gpus_to_use = "0,1"
+gpus_to_use = "0"
 os.environ["CUDA_VISIBLE_DEVICES"] = gpus_to_use
-device = torch.device("cuda:"+gpus_to_use if torch.cuda.is_available() else "cpu")
+device = torch.device(f"cuda:{gpus_to_use}" if torch.cuda.is_available() else "cpu")
+# ! does not work with multiple gpus: invalid device string
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CACHE_DIR = os.path.join(PROJECT_DIR, '.cache')
@@ -39,25 +39,98 @@ def load_codellama():
         #device_map='auto',
         cache_dir=CACHE_DIR,
     )
-    model.to(device)
-    print(model.device)
-    print(device)
 
+    print(f"Loaded model on device: {device} | {model.device}")
     return model, tokenizer
 
-'''
 
-template_str = json.load(open(template_path, "r"))['v3']
-template = PromptTemplate.from_template(template_str)
 
-output_parser, format_instructions = output.build_parser(schemas_version="v3",
-                                                         parser_type="structured",
-                                                         only_json=False)
+def tokenize_instructions_and_examples(few_shot:bool, tokenizer, instructions):
+    if few_shot:
+        examples = ['ex']
+        examples_tokenized = tokenizer(examples, return_tensors="pt").to(device)
+    else:
+        examples_tokenized = None
 
-responses = json.load(open(responses_path, "r"))
-responses = {int(idx): response.strip('\n') for idx, response in responses.items()}
+    instructions_tokenized = tokenizer(instructions, return_tensors="pt").to(device)
 
-'''
+    return instructions_tokenized, examples_tokenized
+
+
+
+def tokenize_inputs(inputs, tokenizer):
+    tokenized_inputs = []
+    for input in inputs:
+        tokenized_inputs.append(
+            tokenizer(input, return_tensors="pt").to(device)
+    )
+    return tokenized_inputs
+
+
+def create_batch_inputs(instructions_tok, examples_tok, inputs_tok):
+
+    if examples_tok: # append examples to instructions
+        instructions_tok = torch.cat(
+            [instructions_tok['input_ids'],
+            examples_tok['input_ids']],
+            dim=1
+        )
+    
+    combined_input_ids = [torch.cat(
+        [instructions_tok['input_ids'],
+        input_tok['input_ids']],
+        dim=1) for input_tok in inputs_tok
+    ]
+
+    combined_attention_mask = [torch.cat(
+        [instructions_tok['attention_mask'],
+        input_tok['attention_mask']],
+        dim=1) for input_tok in inputs_tok
+    ]
+
+    # pad all tensors to have same length
+    max_len = max([x.squeeze().numel() for x in combined_input_ids])
+
+    padded_input_ids = [torch.nn.functional.pad(
+        tensor, 
+        pad=(0, max_len - tensor.numel()), 
+        mode='constant', 
+        value=0
+    ) for tensor in combined_input_ids]
+
+    padded_attention_mask = [torch.nn.functional.pad(
+        tensor,
+        pad=(0, max_len - tensor.numel()),
+        mode='constant',
+        value=0
+    ) for tensor in combined_attention_mask]
+
+    batch_encoded = BatchEncoding(
+        {'input_ids': torch.stack(padded_input_ids).squeeze(1),
+         'attention_mask': torch.stack(padded_attention_mask).squeeze(1)}
+        )
+    
+    return batch_encoded
+
+    
+
+def run_batch(model, batch_encoded):
+    return model.generate(
+        **batch_encoded,
+        do_sample=False,
+        max_new_tokens=50,
+        top_p=0.5,
+        temperature=0.1,
+    )
+
+
+def decode_batch(batch_outputs, batch_inputs, tokenizer):
+    return tokenizer.batch_decode(
+        batch_outputs[:, batch_inputs['input_ids'].shape[1]:],
+        skip_special_tokens=True
+    )
+
+
 
 if __name__ == "__main__":
     load_codellama()
