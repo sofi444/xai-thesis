bb221d475ffcd07577adc1cee695e8133af32fa3
sofi444 <sofyasr22@icloud.com>
2023-09-23 09:44:52 +0200

    parse and eval 10000

--- eval_predictions.py
+++ eval_predictions.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
                         help="parsed responses file to evaluate: rel path | file name")
     parser.add_argument("--split",
                         type=str,
-                        help="split to evaluate on: train | dev | test")
+                        help="split to evaluate on: train | dev | test | merged")
     
     args = parser.parse_args()
     evaluate_CoQA(args)
