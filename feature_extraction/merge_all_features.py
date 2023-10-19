9aae68b8823effac7de38c29b2372e024270dd24
sofi444 <sofyasr22@icloud.com>
2023-10-19 15:36:25 +0200

    add args, new path for features

--- feature_extraction/merge_all_features.py
+++ feature_extraction/merge_all_features.py
@@ -1,38 +1,66 @@
 
 import os
 import pandas as pd
+import argparse
 
 
-
+# Directories
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-OLD_FEATURES_DIR = os.path.join(PROJECT_DIR, "feature_extraction/featureExtraction/output/")
-ARG_FEATURES_DIR = os.path.join(PROJECT_DIR, "feature_extraction/ArgQualityAdapters/")
-
-run_id = "12091031"
+FEATURES_DIR = os.path.join(PROJECT_DIR, "feature_extraction/features/")
 
 
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--trad_features_file", 
+                    type=str, 
+                    required=True, 
+                    help="File with traditional features (can pass path or filename)") 
+parser.add_argument("--arg_features_file",
+                    type=str, 
+                    required=True,
+                    help="File with arg features (can pass path or filename)")
+args = parser.parse_args()
 
-old_df = pd.read_csv(
-    os.path.join(OLD_FEATURES_DIR, f"{run_id}_features.csv.gz"), 
-    compression='gzip'
-)
+
+# Get run id and make sure it is the same for both files
+run_id = args.trad_features.split("/")[-1].split("_")[0]
+try:
+    assert run_id == args.arg_features.split("/")[-1].split("_")[0]
+except AssertionError:
+    print("\n! Run id mismatch; you are trying to merge features from different data")
+    exit()
+
+# Set paths (can pass path or filename)
+if os.path.exists(args.trad_features_file):
+    trad_path = args.trad_features_file
+else:
+    trad_path = os.path.join(FEATURES_DIR, args.trad_features_file)
+
+if os.path.exists(args.arg_features_file):
+    arg_path = args.arg_features_file
+else:
+    arg_path = os.path.join(FEATURES_DIR, args.arg_features_file)
+
+# Read data
+trad_df = pd.read_csv(
+    trad_path, compression='gzip'
+    ) if trad_path.endswith(".gz") else pd.read_csv(trad_path)
 
 arg_df = pd.read_csv(
-    os.path.join(ARG_FEATURES_DIR, f"{run_id}_arg_features_all.csv.gz"),
-    compression='gzip'
-)
+    arg_path, compression='gzip'
+    ) if arg_path.endswith(".gz") else pd.read_csv(arg_path)
 
-# merge
+# Merge
 merged_df = pd.merge(
-    old_df, arg_df, 
+    trad_df, arg_df, 
     how='left', 
     left_on='idx', 
     right_on='idx'
 )
 
-# save
+# Save
 merged_df.to_csv(
-    os.path.join(PROJECT_DIR, "feature_extraction", f"{run_id}_all_features.csv.gz"),
+    os.path.join(FEATURES_DIR, f"{run_id}_all_features.csv.gz"),
     index=False,
     compression='gzip'
 )
